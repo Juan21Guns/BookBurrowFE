480f6215e6ef500ec14ae021699047df1e6688f0
Juan21Guns <reelyvaughn@gmail.com>
2024-05-23 20:04:41 -0600

    final commit for the night

--- src/pages/AWS/SignIn.tsx
+++ src/pages/AWS/SignIn.tsx
@@ -1,12 +1,43 @@
+import { signIn, signOut } from "aws-amplify/auth";
+import { useNavigate } from "react-router-dom";
+
 function SignIn () {
+    const navigate = useNavigate();
+
+    async function handleClick (e: any) {
+        e.preventDefault();
+        const form = e.currentTarget;
+        console.log(form.elements);
+
+        try {
+            await signOut();
+        } catch (error) {
+            console.log();
+        }
+
+        try {
+            const { nextStep } = await signIn({
+                username: form.elements.lusername.value,
+                password: form.elements.lpassword.value,
+            });
+            
+            if (nextStep.signInStep === "DONE") {
+                navigate("/home");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
-            <form>
-                <label htmlFor="username">Email</label>
-                <input type="text" placeholder="your-email@example.com"></input>
+            <form onSubmit={handleClick}>
+                <label htmlFor="lusername">Email</label>
+                <input type="text" id="lusername" name="lusername" placeholder="your-email@example.com"></input>
 
-                <label htmlFor="password">Password</label>
-                <input type="text" placeholder="password (shhhh)"></input>
+                <label htmlFor="lpassword">Password</label>
+                <input type="text" id="lpassword" name="lpassword" placeholder="password (shhhh)"></input>
+                <button>Submit</button>
             </form>
         </>
     )
