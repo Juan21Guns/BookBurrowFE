d05f660a33fe5f64ccf50b6baf63f4fb00bc9c0f
Juan21Guns <reelyvaughn@gmail.com>
2024-05-30 17:12:21 -0600

    created functionality for bookrecs

--- src/pages/BookRecs/BookRecs.tsx
+++ src/pages/BookRecs/BookRecs.tsx
@@ -11,40 +11,61 @@ function BookRecs () {
     const todos = useSelector((state: IRootState) => state.bookRec.todos);
 
     const [books, setBooks] = React.useState(todos);
+    const [start, setStart] = React.useState(0);
+    const [end, setEnd] = React.useState(5);
 
     useEffect(() => {
       removeBooks();
-      getBooks();
+      getBooks(start, end);
+      setStart(start + 5);
+      setEnd(end + 5);
     }, [])
 
     useEffect(() => {
       setBooks(todos);
     }, [todos])
 
+    async function handleClick () {
+      setStart(start + 5);
+      setEnd(end + 5);
+      const isNull = await getBooks(start, end);
+      
+      if (isNull.length == 0) {
+        const button = document.getElementById("show-more");
+        button?.setAttribute("disabled", "");
+      }
+    }
+
     const removeBooks = () => {
       dispatch(removeTodo());
     }
 
-    const getBooks = () => {
+    const getBooks = (s: number, e: number) => {
       //book searches 
-      axios.get(`${import.meta.env.VITE_SERVER}/getOtherBooks?start=0&end=2`)
+      return axios.get(`${import.meta.env.VITE_SERVER}/getOtherBooks?start=${s}&end=${e}`)
           .then((response: any) => {
             response.data.forEach((element: any) => {
               dispatch(addTodo(element));
             });
+            
+            return response.data;
           })
           .catch((err: Error) => {
             console.log(err);
           })
           .finally()
-    };
+    }
 
     return (
         <div className="bookRecs">
             <h1>See what others are reading!</h1>
-            {books.map((book: any) => {
-              return <Books book={book} key={book.bookISBN} />
-            })}
+              {books.map((book: any) => {
+                return (
+                    <Books book={book} key={book.bookISBN} />
+                )
+              })}
+
+              <button id="show-more" onClick={handleClick}>Show More</button>
         </div>
     )
 }
