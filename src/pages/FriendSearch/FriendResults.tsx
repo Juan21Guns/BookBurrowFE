f59f776e07dacd3ba7a292eea9548e568a72ca63
Juan21Guns <reelyvaughn@gmail.com>
2024-06-03 14:17:05 -0600

    patched bug with useeffect

--- src/pages/FriendSearch/FriendResults.tsx
+++ src/pages/FriendSearch/FriendResults.tsx
@@ -2,26 +2,42 @@ import { useSelector } from "react-redux";
 import { IRootState } from "../../redux/IRootState";
 import React, { useEffect } from "react";
 
+interface friend {
+    userId: number,
+    firstName: string,
+    lastName: string,
+    friendStatus: number,
+}
+
+type Dictionary = { [index: string]: string }
+
 function FriendResults () {
     const todos = useSelector((state: IRootState) => state.friend.friends);
+    const filtered = todos.filter((e: any) => e.friendStatus != 3);
+    const [friends, setFriends] = React.useState(filtered);
 
-    const [friends, setFriends] = React.useState(todos);
-    const [friendStatus, setFriendStatus] = React.useState(0);
 
-    // 0 pending, 1 accepted, 2 denied, 3 blocked
+    const fs = {
+        "-1": "Add Friend",
+        "0": "Pending",
+        "1": "Accepted",
+        "2": "Denied",
+        "3": "Blocked",
+    } as Dictionary
 
     useEffect(() => {
-        setFriends(todos);
+        setFriends(filtered);
     }, [todos])
 
     return (
         <div id="friend-search-results">
             <h1>Search results:</h1>
-            {friends.map((friend: any) => {
+            {friends.map((friend: friend) => {
+                const fn = fs[friend.friendStatus];
                 return (
                     <div key={friend.userId} id={`friend-${friend.userId}`}>
                         <p>{friend.firstName} {friend.lastName}</p>
-                        <p>{friendStatus}</p>
+                        <p>{fn}</p>
                         <button>Add Friend</button>
                     </div>
                 )
